f8ff0adeb08c054c33298a247db7c05a90e6757e
Pieter Engelbrecht <pgengel@gmail.com>
2017-08-03 22:55:35 +0200

    added list of exceptions to app.settings

--- FileGenerator/BAL/FileGenerator.cs
+++ FileGenerator/BAL/FileGenerator.cs
@@ -4,13 +4,9 @@ using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using Dapper;
-using DocumentFormat.OpenXml.Spreadsheet;
-using DotNetSolidPrinciples.OpenClose;
 using FileGenerator.Models;
 using FileGenerator.Persistance;
-using FileGenerator.Persistance.Factory;
+
 
 namespace FileGenerator.BAL
 {
--- FileGenerator/Program.cs
+++ FileGenerator/Program.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using FileGenerator.BAL;
+using FileGenerator.Persistance;
+using FileGenerator.Persistance.Factory;
+using System.Configuration;
 
 namespace FileGenerator
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-		}
-	}
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      var piiList = new List<string>(ConfigurationManager.AppSettings["pii_exclusions"].Split(new char[] { ';' }));
+      //  BAL.FileGenerator fileGenerator = new BAL.FileGenerator(new CsvDocumentStorage(), new ViewGenerator(new SqlDbConnectionFactory()));
+      //fileGenerator.GenerateFile("", "");
+    }
+  }
 }
